291eb167c8ebda15568520940fe2e4a3e5514330
Nahuel Defossé <nahuel.defosse@gmail.com>
2016-10-18 02:50:09 -0300

    Context processor for bower components

--- webapp/src/main.py
+++ webapp/src/main.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
 
+@app.context_processor
+def utilities():
+    def bower_component(path):
+        return url_for('static', filename='bower_components/{}'.format(path))
+
+    return locals()
+
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
